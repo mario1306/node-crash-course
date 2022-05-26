6aa7dafd8aab701d0d6226655e05489aea0dbf2c
mario1306 <https://github.com>
2022-05-26 11:53:09 +0200

    mk adjust

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+const dbURI = 'mongodb+srv://netninja:gutek123@cluster0.jmspb.mongodb.net/node-tuts';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
